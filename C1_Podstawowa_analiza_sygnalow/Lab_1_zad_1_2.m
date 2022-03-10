4eb78e89c530b568de5d010aa161c74525f3d40b
mgmike1011 <60261042+mgmike1011@users.noreply.github.com>
2022-03-10 16:53:35 +0100

    Poprawa

--- C1_Podstawowa_analiza_sygnalow/Lab_1_zad_1_2.m
+++ C1_Podstawowa_analiza_sygnalow/Lab_1_zad_1_2.m
@@ -1,19 +1,45 @@
 load('StochasticProcess.mat')
+data = StochasticProcess(2:end,:);
+ilosc_realizacji = length(data(:,1));
+czas_realizacji = length(data(1,:));
 % Realizacja w czasie
-m_czas = zeros(1,501);
-sig_kwa_czas = zeros(1,501);
-for i = 1:501
-    x = StochasticProcess(i,:);
+m_czas = zeros(1,czas_realizacji);
+sig_kwa_czas = zeros(1,czas_realizacji);
+for i = 1:czas_realizacji
+    x = data(1:end,i);
     m_czas(i) = mean(x);
     sig_kwa_czas(i) = var(x);
 end
 % Realizacja po realizacji
-m_realizacja = zeros(1,1001);
-sig_kwa_reali = zeros(1,1001);
-for j = 1:1001
-    x = StochasticProcess(:,j);
-    m_czas(j) = mean(x);
-    sig_kwa_czas(j) = var(x);
+m_realizacja = zeros(1,ilosc_realizacji);
+sig_kwa_realizacja = zeros(1,ilosc_realizacji);
+for i = 1:ilosc_realizacji
+    x = data(:,i);
+    m_realizacja(i) = mean(x);
+    sig_kwa_realizacja(i) = var(x);
 end
 % Przedstawienie na wykresie:
-% plot()
+% Wartoœæ oczekiwana
+plot(m_czas)
+hold on
+plot(m_realizacja)
+hold off
+title('Wartoœæ oczekiwana')
+legend('Po czasie','Po realizacji')
+% Wariancja
+figure
+plot(sig_kwa_czas)
+hold on
+plot(sig_kwa_realizacja)
+hold off
+title('Estymatory wariancji')
+legend('Po czasie','Po realizacji')
+% Wyswietlenie w konsoli:
+disp('Wartoœæ oczekiwana po czasie:')
+mean(m_czas)
+disp('Wartoœæ oczekiwana po realizacji:')
+mean(m_realizacja)
+disp('Wariancja po czasie:')
+mean(sig_kwa_czas)
+disp('Wariancja po realizacji:')
+mean(sig_kwa_realizacja)
