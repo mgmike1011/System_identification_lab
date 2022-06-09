0b8748ebacdb428867501125f6fd4e1e8524e66d
mgmike1011 <60261042+mgmike1011@users.noreply.github.com>
2022-06-09 17:33:10 +0200

    SCR _ thread

---
+++ SCR/Watki_zaj_1/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+//############################################
+// Zad 1
+//void foo(){
+//    std::cout<<std::this_thread::get_id()<<std::endl;
+//}
+//#############################################
+int main() {
+    std::cout<<"Podaj liczbe watkow"<<std::endl;
+//#############################################
+// Zad 1
+//    int ilosc =0;
+//    std::cin>>ilosc;
+//    std::cerr << "threads started" << std::endl;
+//    std::vector<std::thread> th_vec;
+//    for(int i=1;i<=ilosc;i++){
+//        th_vec.emplace_back(foo);
+//    }
+//    for(auto &th:th_vec){
+//        th.join();
+//    }
+//#############################################
+// Zad 3
+//    int liczba = 0;
+//    std::vector<std::thread> th_vec;
+//    for(int i=1;i<=1000;i++){
+//        th_vec.emplace_back([&liczba](){
+//            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+//            liczba += 100;
+//        });
+//    }
+//    for(auto &th:th_vec){
+//           th.join();
+//       }
+//    std::cout<<liczba<<std::endl;
+//###########################################
+// Zad 4
+
+    std::cerr << "threads finished" << std::endl;
+
+    return 0;
+}
